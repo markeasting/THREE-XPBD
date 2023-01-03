d806497d4a25bc02e7f11e76a092f89ef9258ad7
Mark <markeasting@gmail.com>
2023-01-03 21:41:38 +0100

    Added static ground to MyScene

--- src/scene/MyScene.ts
+++ src/scene/MyScene.ts
@@ -1,12 +1,23 @@
 import * as THREE from 'three'
 import { PointLight } from '../light/PointLight';
 import { BaseScene } from './BaseScene';
+import { BaseLightingScene } from './BaseLightingScene';
+import { RigidBody } from '../physics/RigidBody';
+import { BoxCollider } from '../physics/Collider';
+import { Vec3 } from '../physics/Vec3';
+import { CoordinateSystem } from '../physics/CoordinateSystem';
 
 export class MyScene extends BaseScene {
+
+    constructor() {
+        super();
+    }
 
     override init() {
         this.camera.position.set(1, 1.5, 1.5)
         this.camera.lookAt(0,0,0);
+
+        // this.insert(new BaseLightingScene);
 
         this.addGeometry();
 
@@ -22,30 +33,55 @@ export class MyScene extends BaseScene {
 
         const ambientLight = new THREE.AmbientLight( 0x404040 );
         this.scene.add( ambientLight );
-        
+
     }
 
     private addGeometry() {
-        const box = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 2, 1), 
-            new THREE.MeshPhongMaterial({
-                color: 0xffffff, 
-            })
-        );
-        this.scene.add( box )
+        // const box = new THREE.Mesh(
+        //     new THREE.BoxGeometry(1, 2, 1),
+        //     new THREE.MeshPhongMaterial({
+        //         color: 0xffffff,
+        //     })
+        // );
+        // box.castShadow = true;
+        // this.scene.add( box )
 
-        const ground = new THREE.Mesh(
-            new THREE.PlaneGeometry(100, 100), 
-            new THREE.MeshPhongMaterial({
-                color: 0x444444, 
-            })
-        );
-        ground.rotation.x = -Math.PI / 2;
-        ground.position.y = -1;
-        this.scene.add( ground )
+        const box = new RigidBody(new THREE.Mesh(
+                new THREE.BoxGeometry(1, 2, 1),
+                new THREE.MeshPhongMaterial({
+                    color: 0xffffff,
+                })
+            ),
+            new BoxCollider(new Vec3(1, 2, 1))
+        )
+        box.pose.p.set(0, 0, 5);
 
-        box.castShadow = true;
-        ground.receiveShadow = true;
+        // const ground = new THREE.Mesh(
+        //     new THREE.PlaneGeometry(100, 100),
+        //     new THREE.MeshPhongMaterial({
+        //         color: 0x444444,
+        //     })
+        // );
+        // ground.rotation.x = -Math.PI / 2;
+        // ground.position.y = -1;
+        // this.scene.add( ground )
+
+        // ground.receiveShadow = true;
+        const ground = new RigidBody(new THREE.Mesh(
+            new THREE.PlaneGeometry(100, 100),
+                new THREE.MeshPhongMaterial({
+                    color: 0xffffff,
+                })
+            ),
+            new BoxCollider(new Vec3(1, 2, 1))
+        )
+        // Ugh:
+        ground.pose.q.setFromEuler(new THREE.Euler(-Math.PI / 2, 0, 0))
+        ground.pose.p.copy(CoordinateSystem.worldToLocal(new Vec3(0, 0, -5), ground.pose.q, ground.pose.p));
+        ground.makeStatic();
+
+        this.addBody(ground);
+
     }
 
 }
