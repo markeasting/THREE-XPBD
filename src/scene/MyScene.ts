f0f3304ed14f178819f88f48c7313096e4c4dd41
markeasting <markeasting@gmail.com>
2023-01-06 09:28:48 +0100

    Random boxes

--- src/scene/MyScene.ts
+++ src/scene/MyScene.ts
@@ -28,11 +28,26 @@ export class MyScene extends BaseScene {
 
     private addGeometry() {
 
-        const b = Box();
-        b.pose.p.set(0, 3, 0);
-        b.pose.q.setFromEuler(new THREE.Euler(Math.random() * Math.PI, Math.random() * Math.PI, Math.random() * Math.PI))
+        // Debug local positions
+        // const boxMesh = new THREE.Mesh(
+        //     new THREE.BoxGeometry(1, 2, 1),
+        //     new THREE.MeshBasicMaterial({
+        //         color: 0xffffff,
+        //         wireframe: true,
+        //     })
+        // );
+        // this.scene.add(boxMesh);
 
-        this.addBody(b);
+        for (let index = 0; index < 10; index++) {
+            const b = Box();
+            b.pose.p.set(
+                Math.random() * 8 - 4, 
+                Math.random() * 4 + 1, 
+                Math.random() * 8 - 4
+            );
+            b.pose.q.setFromEuler(new THREE.Euler(Math.random() * Math.PI, Math.random() * Math.PI, Math.random() * Math.PI))
+            this.addBody(b);
+        }
 
         const ground = new RigidBody(new THREE.Mesh(
             new THREE.PlaneGeometry(0.1, 0.1),
