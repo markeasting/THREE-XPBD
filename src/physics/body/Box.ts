859e8abbdb88a707918ad691a743b30ede371738
Mark <markeasting@gmail.com>
2023-01-09 20:27:37 +0100

    Remove default location from Box()

--- src/physics/body/Box.ts
+++ src/physics/body/Box.ts
@@ -4,7 +4,7 @@ import { RigidBody } from '../RigidBody';
 import { Vec3 } from '../Vec3';
 
 export function Box() {
-        
+
     const boxMesh = new THREE.Mesh(
         new THREE.BoxGeometry(1, 2, 1),
         // new THREE.MeshPhongMaterial({
@@ -22,9 +22,6 @@ export function Box() {
         // new BoxCollider(new Vec3(1, 2, 1))
     )
     box.setBox(new Vec3(1, 2, 1), 1);
-    box.pose.p.set(0, 3, 0);
-    
-    // box.pose.q.setFromEuler(new THREE.Euler(Math.random() * Math.PI, Math.random() * Math.PI, Math.random() * Math.PI))
-    
+
     return box;
 }
