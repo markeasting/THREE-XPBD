373bdf57e06469a9b4061439c4c0aa9cbe9ef537
Mark <markeasting@gmail.com>
2023-01-09 22:16:49 +0100

    Tune materials

--- src/physics/body/Box.ts
+++ src/physics/body/Box.ts
@@ -7,14 +7,14 @@ export function Box() {
 
     const boxMesh = new THREE.Mesh(
         new THREE.BoxGeometry(1, 2, 1),
-        // new THREE.MeshPhongMaterial({
-        //     color: 0xffffff,
-        //     wireframe: true,
-        // }),
-        new THREE.MeshBasicMaterial({
-            color: 0xffffff,
-            wireframe: true,
-        })
+        new THREE.MeshPhongMaterial({
+            // color: 0xffffff,
+            color: 0x00ffcc,
+        }),
+        // new THREE.MeshBasicMaterial({
+        //     color: 0xffffff, // 0x00ffcc,
+        //     // wireframe: true,
+        // })
     );
     const box = new RigidBody(
         boxMesh,
