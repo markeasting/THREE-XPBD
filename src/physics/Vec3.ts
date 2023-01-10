c82051776ce8d517c89f70e600969fe49d9ebf85
Mark <markeasting@gmail.com>
2023-01-10 08:33:42 +0100

    Added methods to Vec3

--- src/physics/Vec3.ts
+++ src/physics/Vec3.ts
@@ -2,7 +2,33 @@ import { Vector3 } from 'three';
 
 export class Vec3 extends Vector3 {
 
+    mul(s: number) {
+        return this.multiplyScalar(s)
+    }
+
+
+    static mul(v: Vec3, s: number) {
+        return v.clone().multiplyScalar(s);
+    }
+
+    static add(v1: Vec3, v2: Vec3) {
+        return new Vec3().addVectors(v1, v2);
+    }
+
+    static sub(v1: Vec3, v2: Vec3) {
+        return new Vec3().subVectors(v1, v2);
+    }
+
+
     static normalize(v: Vec3) {
         return v.clone().normalize();
+    }
+
+    static cross(v1: Vec3, v2: Vec3) {
+        return new Vec3().crossVectors(v1, v2);
+    }
+
+    static dot(v1: Vec3, v2: Vec3) {
+        return v1.dot(v2);
     }
 }
