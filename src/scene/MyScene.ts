ad63427d1661589e911565b20cfa472d3bc127b6
markeasting <markeasting@gmail.com>
2023-01-13 20:03:38 +0100

    Testing combined objects with constraints

--- src/scene/MyScene.ts
+++ src/scene/MyScene.ts
@@ -37,16 +37,6 @@ export class MyScene extends BaseScene {
     }
 
     private addGeometry() {
-
-        // // Debug local positions
-        // const boxMesh = new THREE.Mesh(
-        //     new THREE.BoxGeometry(1, 2, 1),
-        //     new THREE.MeshBasicMaterial({
-        //         color: 0xffffff,
-        //         wireframe: true,
-        //     })
-        // );
-        // this.scene.add(boxMesh);
         
         // for (let index = 0; index < 2; index++) {
         //     const b = Box(1, 2, 1);
@@ -60,9 +50,11 @@ export class MyScene extends BaseScene {
         //     b.pose.q.setFromEuler(new THREE.Euler(Math.random() * Math.PI, Math.random() * Math.PI, Math.random() * Math.PI))
         //     this.addBody(b);
         // }
+        let b0, b1;
 
-        const b0 = Box(2, 1, 0.08).setPos(0, 1.5, 1);
-        const b1 = Box(2, 0.08, 1).setPos(0, 2, 0.5).setStatic();
+        // Hinge
+        b0 = Box(2, 1, 0.08).setPos(0, 1.5, 1);
+        b1 = Box(2, 0.08, 1).setPos(0, 2, 0.5).setStatic();
         this.addBody(b0);
         this.addBody(b1);
 
@@ -70,31 +62,39 @@ export class MyScene extends BaseScene {
             new Constraint(b0, b1)
             .add(new Attachment(new Vec3(0, 0, 0.5), new Vec3(0, 0.5, 0)))
             .add(new AlignAxes)
+        );
+
+
+
+        // Hammer
+        b0 = Box(0.2, 0.2, 7).setPos(0, 2, 3)
+        b1 = Box(2, 1, 1).setPos(0, 2, 0.5)
+        this.addBody(b0);
+        this.addBody(b1);
+
+        this.world.addConstraint(
+            new Constraint(b0, b1)
+            .add(new Attachment(new Vec3(0.1, 0, 0.5), new Vec3(0, 0, -2)))
+            .add(new AlignOrientation)
+        );
+        
+        this.world.addConstraint(
+            new Constraint(b0, b1)
+            .add(new Attachment(new Vec3(0.1, 0, -0.5), new Vec3(0, 0, -3)))
             // .add(new AlignOrientation)
         );
 
-        const ground = new RigidBody(
-            new PlaneCollider(new Vec2(100, 100)),
-            new THREE.Mesh(
-                new THREE.PlaneGeometry(0.1, 0.1, 5, 5),
-                new THREE.MeshPhongMaterial({
-                    color: 0xffffff,
-                    // wireframe: true,
-                })
-            )
-        );
-        ground.pose.q.setFromEuler(new THREE.Euler(-Math.PI / 2, 0, 0))
-        ground.pose.p.copy(new Vec3(0, 0, 0));
-        ground.setStatic();
 
-        this.addBody(ground);
+
+
+        this.addGround();
 
     }
 
     public update(time: number, dt: number, keys: Record<string, boolean>): void {
 
         if (keys.KeyQ) {
-            const body = this.world.bodies[0];
+            const body = this.world.bodies[1];
             const tetraPointL = new Vec3(0, 0, 0.5);
             const tetraPointW = CoordinateSystem.localToWorld(tetraPointL, body.pose.q, body.pose.p);
 
@@ -117,6 +117,24 @@ export class MyScene extends BaseScene {
                 tetraPointW
             );
         }
+    }
+
+    private addGround() {
+        const ground = new RigidBody(
+            new PlaneCollider(new Vec2(100, 100)),
+            new THREE.Mesh(
+                new THREE.PlaneGeometry(0.1, 0.1, 5, 5),
+                new THREE.MeshPhongMaterial({
+                    color: 0xffffff,
+                    // wireframe: true,
+                })
+            )
+        );
+        ground.pose.q.setFromEuler(new THREE.Euler(-Math.PI / 2, 0, 0))
+        ground.pose.p.copy(new Vec3(0, 0, 0));
+        ground.setStatic();
+
+        this.addBody(ground);
     }
 
 }
