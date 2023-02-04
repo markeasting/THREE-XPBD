6a9cc1b20b718b9f26b6c590b91a00d5657fc5bd
markeasting <markeasting@gmail.com>
2023-02-04 13:05:10 +0100

    Added xpbd-wasm testing code

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,31 @@
 import { Game } from './core/Game';
+import * as XPBD from 'xpbd-wasm';
 import { MyScene } from './scene/MyScene';
 import './style.css'
 
 declare global {
     interface Window {
         game: Game,
+        world: XPBD.World
     }
 }
 
 const game = new Game('canvas');
 window.game = game;
 
+let world: XPBD.World;
+
 async function init() {
 
     const scene = new MyScene();
 
     game.add(scene);
+
+    world = await XPBD.init();
+    window.world = world;
+
+    world.addBody(scene.myCube, 1, 3, 1);
+    world.addGround();
 
     update(0);
 }
@@ -26,6 +36,10 @@ function update(time: DOMHighResTimeStamp): void {
     prevTime = time;
 
     game.update(time);
+
+    console.time('wasm');
+    world.update(dt);
+    console.timeEnd('wasm');
 
     requestAnimationFrame(time => {
         update(time);
