0fe082e74ade854f20729bc9e1b90f33bcbf043f
markeasting <markeasting@gmail.com>
2023-02-04 12:30:48 +0100

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import { Game } from './core/Game';
-import * as XPBD from 'xpbd-wasm';
 import { MyScene } from './scene/MyScene';
 import './style.css'
 
 declare global {
     interface Window {
         game: Game,
-        world: XPBD.World
     }
 }
 
 const game = new Game('canvas');
 window.game = game;
-
-let world: XPBD.World;
 
 async function init() {
 
@@ -21,21 +17,15 @@ async function init() {
 
     game.add(scene);
 
-    world = await XPBD.init();
-    window.world = world;
-
-    world.addBody(scene.unitCube);
-
     update(0);
 }
 
-let prevTime, dt;
+let prevTime: number, dt: number;
 function update(time: DOMHighResTimeStamp): void {
-    prevTime = time;
     dt = (time - prevTime) / 1000;
+    prevTime = time;
 
     game.update(time);
-    // world.update(dt);
 
     requestAnimationFrame(time => {
         update(time);
