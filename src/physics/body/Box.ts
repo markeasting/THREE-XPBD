5ec6e6e700b91934186eea713c15bbd00d2da13f
markeasting <markeasting@gmail.com>
2023-02-21 20:46:25 +0100

    Updated box body creation

--- src/physics/body/Box.ts
+++ src/physics/body/Box.ts
@@ -9,22 +9,22 @@ export function Box(width: number = 1.0, height?: number, depth?: number): Rigid
 
     const boxMesh = new THREE.Mesh(
         new THREE.BoxGeometry(width, height, depth),
-        // new THREE.MeshPhongMaterial({
-        //     // color: 0xffffff,
-        //     // color: 0x00ffcc,
-        //     color: new THREE.Color().setHSL(0.5, 1, 0.5),
-        // }),
-        new THREE.MeshBasicMaterial({
-            color: 0xffffff, // 0x00ffcc,
-            wireframe: true,
-            wireframeLinewidth: 2
-        })
+        new THREE.MeshPhongMaterial({
+            // color: 0xffffff,
+            // color: 0x00ffcc,
+            color: new THREE.Color().setHSL(0.5, 1, 0.5),
+            // polygonOffset: true,
+            // polygonOffsetFactor: 1, // Gives neater wireframes
+            // polygonOffsetUnits: 1
+        }),
+        // new THREE.MeshBasicMaterial({
+        //     color: 0xffffff, // 0x00ffcc,
+        //     wireframe: true,
+        //     wireframeLinewidth: 2
+        // })
     );
     
-    const box = new RigidBody(
-            new MeshCollider()
-                .setGeometry('box', width, height, depth)
-        )
+    const box = new RigidBody(new MeshCollider().setGeometry(boxMesh.geometry))
         .setMesh(boxMesh)
         .setBox(new Vec3(width, height, depth), 1)
 
